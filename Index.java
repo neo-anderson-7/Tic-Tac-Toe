73cfaa744b77017727f87987d52d3138945c34dd
neo-anderson-7 <haydengunk@gmail.com>
2020-02-22 20:21:29 +0530

    Main-init

---
+++ Index.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Index {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+            coordinates will be a{1,2,3}, b{1,2,3}, c{1,2,3}
+            code 0 stands for unfilled block, 1 for player, 2 for computer
+            Layout:
+
+                a b c
+            1 | X X X |
+            2 | X X X |
+            3 | X X X |
+        */
+
+        int a1 = 0, a2 = 0, a3 = 0, b1 = 0, b2 = 0, b3 = 0, c1 = 0, c2 = 0, c3 = 0;
+        
+
+        scanner.close();
+    }
+}
