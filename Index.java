9287f86e2fbd952bc52f5a4c018ca17718c33ed2
neo-anderson-7 <haydengunk@gmail.com>
2020-02-29 10:34:52 +0530

    Board set

--- Index.java
+++ Index.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Index {
-//public static String[] 
+public static String[][][] locs = {{{" ", "a1"}, {" ", "b1"}, {" ", "c1"}}, 
+                                        {{" ", "a2"}, {" ", "b2"}, {" ", "c2"}},
+                                        {{" ", "a3"}, {" ", "b3"}, {" ", "c3"}}};   // Locations
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,5 +22,12 @@ public class Index {
         
 
         scanner.close();
+    }
+
+    public static void board() {
+        System.out.println("    a b c ");
+        System.out.println("1 | " + locs[0][0][0] + " " + locs[0][1][0] + " " + locs[0][2][0] + " |");
+        System.out.println("2 | " + locs[1][0][0] + " " + locs[1][1][0] + " " + locs[1][2][0] + " |");
+        System.out.println("3 | " + locs[2][0][0] + " " + locs[2][1][0] + " " + locs[2][2][0] + " |");
     }
 }
